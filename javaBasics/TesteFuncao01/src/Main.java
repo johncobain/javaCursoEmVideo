6730c6837887fc554be7e09e9cf00b55d1d33822
Andrey <andreygdsn@gmail.com>
2025-03-09 16:49:42 -0300

    metodos

---
+++ javaBasics/TesteFuncao01/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    static int soma(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Começo do programa");
+        int sm = soma(5,2);
+        System.out.println("A soma de 5 + 2 = " + sm);
+    }
+}
---
+++ javaBasics/TesteFuncao02/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Vai começar a contagem");
+        System.out.println(Operacoes.contador(1,5));
+    }
+}
