9e3da1bdbe3f122e36f90d371bc13d9dab82466c
Andrey <andreygdsn@gmail.com>
2025-03-12 19:46:06 -0300

    getters setters e constructor

---
+++ pooJava/Aula04/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Bic", "Azul", 0.5f);
+        c1.status();
+
+        Caneta c2 = new Caneta("Nic", "Vermelha", 0.7f);
+    }
+}
