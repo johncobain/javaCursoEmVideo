e88738210fa47b6e64b5b415732348f38206eafd
Andrey <andreygdsn@gmail.com>
2025-03-19 21:07:49 -0300

    polimorfismo pt1

---
+++ pooJava/Polimorfismopt1/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+//        Animal n = new Animal();
+        Mamifero m = new Mamifero();
+        Ave a = new Ave();
+        Peixe p = new Peixe();
+        Reptil r = new Reptil();
+
+        Canguru c = new Canguru();
+        Cachorro k = new Cachorro();
+        Cobra j = new Cobra();
+        Tartaruga t = new Tartaruga();
+        GoldFish g = new GoldFish();
+        Arara e = new Arara();
+
+        c.locomover();
+        k.locomover();
+        c.emitirSom();
+        k.emitirSom();
+
+
+//        m.setPeso(35.3f);
+//        m.setCorPelo("Marrom");
+//        m.alimentar();
+//        m.locomover();
+//        m.emitirSom();
+//
+//        a.setPeso(0.9f);
+//        a.setCorPena("Branco");
+//        a.alimentar();
+//        a.locomover();
+//        a.emitirSom();
+//        a.fazerNinho();
+//
+//        p.setPeso(0.4f);
+//        p.setCorEscama("Cinza");
+//        p.alimentar();
+//        p.locomover();
+//        p.emitirSom();
+//        p.soltarBolha();
+//
+//        r.setPeso(1.5f);
+//        r.setCorEscama("Laranja");
+//        r.alimentar();
+//        r.locomover();
+//        r.emitirSom();
+    }
+}
