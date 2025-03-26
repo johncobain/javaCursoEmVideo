7446f2be097bccb9770556443c4e26a16a52209a
Andrey <andreygdsn@gmail.com>
2025-03-25 21:47:14 -0300

    projeto youtube pt2

--- pooJava/ProjetoYoutube/src/Main.java
+++ pooJava/ProjetoYoutube/src/Main.java
@@ -9,8 +9,24 @@ public class Main {
         g[0] = new Gafanhoto("Jubileu", 15, "M", "juba");
         g[1] = new Gafanhoto("Creuza", 15, "F", "creuzita");
 
-        System.out.println(v[0].toString());
+        Visualizacao[] vis =new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]);
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+        vis[1] = new Visualizacao(g[0], v[1]);
+        vis[1].avaliar(87.0f);
+        System.out.println(vis[1].toString());
+
         System.out.println();
-        System.out.println(g[0].toString());
+        System.out.println("VÍDEOS=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        for (Video video : v) {
+            System.out.println(video.toString());
+        }
+
+        System.out.println("GAFANHOTOS=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        for (Gafanhoto gafanhoto : g) {
+            System.out.println(gafanhoto.toString());
+        }
     }
 }
